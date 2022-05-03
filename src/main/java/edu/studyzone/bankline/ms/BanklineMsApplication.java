a5e2a6d3534b4d03ab621288858fc6b6294e1f63
Caio Roberto <caio.roberto@sensedia.com>
2022-05-03 10:28:39 -0300

    project init

---
+++ src/main/java/edu/studyzone/bankline/ms/BanklineMsApplication.java
@@ -0,0 +1,12 @@
+package edu.studyzone.bankline.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineMsApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/studyzone/bankline/ms/BanklineMsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.studyzone.bankline.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
